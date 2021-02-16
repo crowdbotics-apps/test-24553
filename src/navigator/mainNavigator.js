f9b8d1f97bb85a3a8cc94d19dc8781576ae434ac
Crowdbotics <team@crowdbotics.com>
2021-02-16 08:08:21 +0000

    add package blueprint-react-native-addItem-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Additem205733Navigator from '../features/Additem205733/navigator';
 import Maps205729Navigator from '../features/Maps205729/navigator';
 import UserProfile205725Navigator from '../features/UserProfile205725/navigator';
 
@@ -15,6 +16,7 @@ import UserProfile205725Navigator from '../features/UserProfile205725/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Additem205733: { screen: Additem205733Navigator },
 Maps205729: { screen: Maps205729Navigator },
 UserProfile205725: { screen: UserProfile205725Navigator },
 
